cdd6c59ae8b0e472d3028f6e44f360ed4c17df82
Kayumiy <qayum.mus@mail.ru>
2020-09-27 16:35:12 +1000

    Add project

---
+++ Task1/Assets/Scripts/Bubble.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    float increasingVal;            // increasing value for bubble. Bubble should be increased every second
+    public float radius;
+    public string nameOfBubble;
+    public GameManager gameManager;   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        GetIncreasingValue();
+        GetRandomColor();
+        InvokeRepeating("IncreaseBubbleSize", 1f, 1f);
+        CheckDistanceWithAllCircles();
+        CheckBordersColission();
+    }
+
+
+    /// <summary>
+    /// Give random color to buble
+    /// </summary>
+    public void GetRandomColor()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(
+        UnityEngine.Random.Range(0f, 1f),
+        UnityEngine.Random.Range(0f, 1f),
+        UnityEngine.Random.Range(0f, 1f));
+    }
+
+    /// <summary>
+    /// Calculate increasing value based on the size of the circle
+    /// </summary>
+    public void GetIncreasingValue()
+    {
+        increasingVal = radius * 10 / 100;
+    }    
+
+    /// <summary>
+    /// Increase size of the bubble every second
+    /// </summary>
+    public void IncreaseBubbleSize()
+    {
+        transform.localScale += new Vector3(increasingVal, increasingVal);
+        CheckDistanceWithAllCircles();
+        CheckBordersColission();
+
+    }
+
+    /// <summary>
+    /// If the collision happens, Destroy both bubbles and create new one
+    /// </summary>
+    public void CheckDistanceWithAllCircles()
+    {
+        foreach (GameObject bubbleObj in gameManager.bubbleGroup.ToList())
+        {            
+            Bubble aBubble = bubbleObj.GetComponent<Bubble>();
+            // Don't calculate the distance between bubble and itself
+            if (nameOfBubble != aBubble.nameOfBubble)
+            {                
+                //distance =  sqrt((x2 − x1)^2 + (y2 − y1)^2) − (r2 + r1)    this formula is the distance between bubbles
+                double distance = (float)Math.Sqrt((transform.position.x - bubbleObj.transform.position.x) * (transform.position.x - bubbleObj.transform.position.x) +
+                    (transform.position.y - bubbleObj.transform.position.y) * (transform.position.y - bubbleObj.transform.position.y));
+                distance = distance - (transform.localScale.x / 2 + bubbleObj.transform.localScale.x / 2);
+                if (distance <= 0)
+                {
+                    float x = ((aBubble.transform.localScale.x / 2 * (transform.position.x - aBubble.transform.position.x)) /
+                        (transform.localScale.x / 2 +  aBubble.transform.localScale.x / 2)) + aBubble.transform.position.x;
+                    float y = ((aBubble.transform.localScale.y / 2 * (transform.position.y - aBubble.transform.position.y)) /
+                        (transform.localScale.x / 2 + aBubble.transform.localScale.x / 2)) + aBubble.transform.position.y;
+                    float rad = transform.localScale.x / 2  + aBubble.transform.localScale.x /2;                    
+                    gameManager.CreateBubbleOnCollisionPoint(x, y, rad, this.gameObject, bubbleObj);
+                }
+            }
+        }
+    }
+   
+    /// <summary>
+    /// If bubble collide with border, destroy bubble
+    /// </summary>
+    public void CheckBordersColission()
+    {
+        if (transform.position.x > 0 && transform.position.y < 0)
+        {
+            if (transform.position.x + transform.localScale.x / 2 >= gameManager.rectWidth/2)
+            {
+                gameManager.DestroyBubble(nameOfBubble, gameObject);               
+            }
+            else if (transform.position.y - transform.localScale.y / 2 <= -gameManager.rectHeight/2)
+            {
+                gameManager.DestroyBubble(nameOfBubble, gameObject);                
+            }
+        }
+        else if (transform.position.x > 0 && transform.position.y > 0)
+        {
+            if (transform.position.y + transform.localScale.y / 2 >= gameManager.rectHeight / 2)
+            {
+                gameManager.DestroyBubble(nameOfBubble, gameObject);               
+            }
+            else if (transform.position.x + transform.localScale.x / 2 >= gameManager.rectWidth / 2)
+            {
+                gameManager.DestroyBubble(nameOfBubble, gameObject);                
+            }
+        }
+        else if (transform.position.x < 0 && transform.position.y > 0)
+        {
+            if (transform.position.x - transform.localScale.x / 2 <= -gameManager.rectWidth / 2)
+            {
+                gameManager.DestroyBubble(nameOfBubble, gameObject);               
+            }
+            else if (transform.position.y + transform.localScale.y / 2 >= gameManager.rectHeight / 2)
+            {
+                gameManager.DestroyBubble(nameOfBubble, gameObject);                
+            }
+        }
+        else
+        {
+            if (transform.position.x - transform.localScale.x / 2 <= -gameManager.rectWidth / 2)
+            {
+                gameManager.DestroyBubble(nameOfBubble, gameObject);                
+            }
+            else if (transform.position.y - transform.localScale.y / 2 <= -gameManager.rectHeight / 2)
+            {
+                gameManager.DestroyBubble(nameOfBubble, gameObject);                
+            }
+        }
+    }
+
+}
---
+++ Task1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{    
+    // Input Data 
+    public int objectCount;         // the initial number of bubbles in the field
+    public float rectWidth;         // the size of the playing field
+    public float rectHeight;        // the size of the playing field
+    public float objectMinRadius;   // the range of values ​​for the bubble's initial radius
+    public float objectMaxRadius;   // the range of values ​​for the bubble's initial radius
+    
+    public List<GameObject> bubbleGroup;     // collection of bubbles
+    public GameObject rectangle;                    // Rectangle field
+    public GameObject bubble;                       // bubble prefab
+    float bubbleRadius;                 // random radius value for each new created bubble
+    int number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 0;
+        ResizeRect();
+        bubbleGroup = new List<GameObject>();
+        CreateInitialBubbles();
+        InvokeRepeating("CreateBubblePerThreeSec", 3f, 3f);
+    }
+    
+
+    /// <summary>
+    /// Get random radiues value for bubble
+    /// </summary>
+    public void GetRandomRadiusValue()
+    {
+        bubbleRadius = UnityEngine.Random.Range(objectMinRadius, objectMaxRadius);
+    }
+
+    /// <summary>
+    /// Create initial bubbles in the field after playing Scene and add it to the collection
+    /// </summary>
+    public void CreateInitialBubbles()
+    {
+        for (int i = 0; i < objectCount; i++)
+        {
+            CheckSpaceIsEmpty();
+            GetRandomRadiusValue();
+            //CreateSingleBubble();
+        }
+    }
+
+    /// <summary>
+    /// Create a bubble with instantioation prefab  
+    /// </summary>
+    public void CreateSingleBubble( float x, float y)
+    {
+        number++;
+        GameObject aBubble = Instantiate(bubble, new Vector3(x , y , 1), Quaternion.identity) as GameObject;
+        //GetRandomRadiusValue();
+        aBubble.transform.localScale = new Vector3(bubbleRadius, bubbleRadius);
+        aBubble.GetComponent<Bubble>().radius = bubbleRadius;
+        aBubble.GetComponent<Bubble>().nameOfBubble = number.ToString();
+        bubbleGroup.Add(aBubble);
+    }
+
+    /// <summary>
+    /// Find empty space to create bubble in the field
+    /// </summary>
+    public void CheckSpaceIsEmpty()
+    {        
+        bool isEmpty = false;
+        // While loop runs until finding the empty space in the field
+        while (!isEmpty)
+        {
+            float x = UnityEngine.Random.Range(-rectWidth / 2 + objectMaxRadius, rectWidth / 2 - objectMaxRadius);
+            float y = UnityEngine.Random.Range(-rectHeight / 2 + objectMaxRadius, rectHeight / 2 - objectMaxRadius);
+
+            if (bubbleGroup.Count > 0)
+            {
+                int n = 0;
+                // Check random position can collide with other bubble's position 
+                foreach (GameObject bubbleObj in bubbleGroup)
+                {                    
+                    Bubble aBubble = bubbleObj.GetComponent<Bubble>();
+                    //distance =  sqrt((x2 − x1)^2 + (y2 − y1)^2) − (r2 + r1)    this formula is the distance between bubbles
+                    double distance = (float)Math.Sqrt((x - bubbleObj.transform.position.x) * (x - bubbleObj.transform.position.x) +
+                        (y - bubbleObj.transform.position.y) * (y - bubbleObj.transform.position.y));
+                    distance = distance - (bubbleRadius + bubbleObj.transform.localScale.x / 2);
+                    if (distance > 0)
+                    {
+                        n++;                       
+                    }
+                    else if(distance <= 0)
+                    {
+                        break;
+                    }
+                }
+                if (n == bubbleGroup.Count)
+                {
+                    isEmpty = true;
+                    CreateSingleBubble(x, y); // x and y position ni ber
+                }
+            }
+            else
+            {
+                isEmpty = true;
+                CreateSingleBubble(x, y); // x and y position ni ber
+            }
+        }        
+    }
+
+    /// <summary>
+    /// Create new bubble on collision point
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="radius"></param>
+    /// <param name="firstObj"></param>
+    /// <param name="secondObj"></param>
+    public void CreateBubbleOnCollisionPoint(float x, float y, float radius, GameObject firstObj, GameObject secondObj)
+    {
+        DestroyBubble(firstObj.GetComponent<Bubble>().nameOfBubble, firstObj);
+        DestroyBubble(secondObj.GetComponent<Bubble>().nameOfBubble, secondObj);
+        number++;
+        GameObject aBubble = Instantiate(bubble, new Vector3(x, y, 1), Quaternion.identity) as GameObject;
+        aBubble.transform.localScale = new Vector3(radius * 2, radius * 2);
+        aBubble.GetComponent<Bubble>().radius = radius;
+        aBubble.GetComponent<Bubble>().nameOfBubble = number.ToString();
+        bubbleGroup.Add(aBubble);
+    }
+
+    /// <summary>
+    /// Resize field based on the use input
+    /// </summary>
+    public void ResizeRect()
+    {
+        rectangle.transform.localScale = new Vector3(rectWidth, rectHeight);
+    }
+
+    /// <summary>
+    /// Create new bubble object every three second and add it to the collection 
+    /// </summary>
+    public void CreateBubblePerThreeSec()
+    {
+        CheckSpaceIsEmpty();
+        GetRandomRadiusValue();
+    }
+
+    /// <summary>
+    /// Destroy a bubble from Scene and List collection
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="bubbleObj"></param>
+    public void DestroyBubble(string name, GameObject bubbleObj)
+    {
+        foreach (GameObject gmObj in bubbleGroup)
+        {
+            if (gmObj.GetComponent<Bubble>() != null)
+            {
+                Bubble bubble = gmObj.GetComponent<Bubble>();
+                if (name.Equals(bubble.nameOfBubble))
+                {
+                    bubbleGroup.Remove(gmObj);
+                    break;
+                }
+            }
+        }
+        Destroy(bubbleObj);
+    }
+
+}
